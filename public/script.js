643439afc35e6ead370b4ea068521e2ae9b0a3b9
qhollis <58087502+qhollis@users.noreply.github.com>
2021-03-26 17:31:31 -0400

    lab 6 attempt 2

--- public/script.js
+++ public/script.js
@@ -1,5 +1,30 @@
-async function diningData() {
-    // fetch request
-    const request = await fetch('/api/dining');
-}
+async function dining(){
+    const requestdata = await fetch('/api/dining');
+    const diningdata = await requestdata.json();
+    const arraydata = diningdata.data;
+    console.log(arraydata);
+    console.table(diningdata);
+    const targettable = document.querySelector('.table');
+
+  
+
+    arraydata.forEach((element) => {
+      const appendelement = document.createElement('tr');
+      appendelement.innerHTML = `
+        <td>${element.hall_id}</td>
+        <td>${element.hall_name}</td>
+        <td>${element.hall_address}</td>
+      </tr>
+    `;
+      targettable.append(appendelement); 
+    });
+  }
+
+  async function windowActions() {
+    console.log('loaded window');
+    const data = await dining();
+    console.table(data);
+  }
+
+  window.onload = windowActions;
 
